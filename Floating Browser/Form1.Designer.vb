7ed637dad7dce45a80eb9cad3a707bd35f3fc925
Scott <fma96580@gmail.com>
2014-05-15 17:57:52 +0100

    Initial Alpha Release
    Only supports fullscreen site on youtube.

---
+++ Floating Browser/Form1.Designer.vb
@@ -0,0 +1,220 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.NsControlButton3 = New FloatingBrowser.NSControlButton()
+        Me.NsControlButton2 = New FloatingBrowser.NSControlButton()
+        Me.NsControlButton1 = New FloatingBrowser.NSControlButton()
+        Me.NsTrackBar1 = New FloatingBrowser.NSTrackBar()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.WebBrowser1.IsWebBrowserContextMenuEnabled = False
+        Me.WebBrowser1.Location = New System.Drawing.Point(0, 0)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.ScriptErrorsSuppressed = True
+        Me.WebBrowser1.ScrollBarsEnabled = False
+        Me.WebBrowser1.Size = New System.Drawing.Size(410, 249)
+        Me.WebBrowser1.TabIndex = 0
+        Me.WebBrowser1.Url = New System.Uri("about:blank", System.UriKind.Absolute)
+        Me.WebBrowser1.WebBrowserShortcutsEnabled = False
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 10
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 8.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(6, 5)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(102, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Opacity"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Panel1
+        '
+        Me.Panel1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.NsTrackBar1)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Location = New System.Drawing.Point(0, 254)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(409, 46)
+        Me.Panel1.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI Light", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.DimGray
+        Me.Label3.Location = New System.Drawing.Point(303, 32)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(107, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Created by Cyanlabs"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Panel2
+        '
+        Me.Panel2.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Panel2.Controls.Add(Me.Label2)
+        Me.Panel2.Controls.Add(Me.TextBox1)
+        Me.Panel2.Controls.Add(Me.NsControlButton3)
+        Me.Panel2.Controls.Add(Me.NsControlButton2)
+        Me.Panel2.Controls.Add(Me.NsControlButton1)
+        Me.Panel2.Location = New System.Drawing.Point(0, 0)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(410, 23)
+        Me.Panel2.TabIndex = 5
+        '
+        'Label2
+        '
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(2, 5)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(31, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "URL: "
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(50, Byte), Integer), CType(CType(50, Byte), Integer), CType(CType(50, Byte), Integer))
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox1.ForeColor = System.Drawing.Color.White
+        Me.TextBox1.Location = New System.Drawing.Point(34, 2)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(282, 20)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.Text = "about:floatingbrowser"
+        '
+        'NsControlButton3
+        '
+        Me.NsControlButton3.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.NsControlButton3.ControlButton = FloatingBrowser.NSControlButton.Button.MaximizeRestore
+        Me.NsControlButton3.Location = New System.Drawing.Point(371, 2)
+        Me.NsControlButton3.Margin = New System.Windows.Forms.Padding(0)
+        Me.NsControlButton3.MaximumSize = New System.Drawing.Size(18, 20)
+        Me.NsControlButton3.MinimumSize = New System.Drawing.Size(18, 20)
+        Me.NsControlButton3.Name = "NsControlButton3"
+        Me.NsControlButton3.Size = New System.Drawing.Size(18, 20)
+        Me.NsControlButton3.TabIndex = 0
+        Me.NsControlButton3.Text = "NsControlButton1"
+        '
+        'NsControlButton2
+        '
+        Me.NsControlButton2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.NsControlButton2.ControlButton = FloatingBrowser.NSControlButton.Button.Minimize
+        Me.NsControlButton2.Location = New System.Drawing.Point(352, 2)
+        Me.NsControlButton2.Margin = New System.Windows.Forms.Padding(0)
+        Me.NsControlButton2.MaximumSize = New System.Drawing.Size(18, 20)
+        Me.NsControlButton2.MinimumSize = New System.Drawing.Size(18, 20)
+        Me.NsControlButton2.Name = "NsControlButton2"
+        Me.NsControlButton2.Size = New System.Drawing.Size(18, 20)
+        Me.NsControlButton2.TabIndex = 0
+        Me.NsControlButton2.Text = "NsControlButton1"
+        '
+        'NsControlButton1
+        '
+        Me.NsControlButton1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.NsControlButton1.ControlButton = FloatingBrowser.NSControlButton.Button.Close
+        Me.NsControlButton1.Location = New System.Drawing.Point(388, 2)
+        Me.NsControlButton1.Margin = New System.Windows.Forms.Padding(0)
+        Me.NsControlButton1.MaximumSize = New System.Drawing.Size(18, 20)
+        Me.NsControlButton1.MinimumSize = New System.Drawing.Size(18, 20)
+        Me.NsControlButton1.Name = "NsControlButton1"
+        Me.NsControlButton1.Size = New System.Drawing.Size(18, 20)
+        Me.NsControlButton1.TabIndex = 0
+        Me.NsControlButton1.Text = "NsControlButton1"
+        '
+        'NsTrackBar1
+        '
+        Me.NsTrackBar1.Location = New System.Drawing.Point(6, 20)
+        Me.NsTrackBar1.Maximum = 100
+        Me.NsTrackBar1.Minimum = 10
+        Me.NsTrackBar1.Name = "NsTrackBar1"
+        Me.NsTrackBar1.Size = New System.Drawing.Size(102, 23)
+        Me.NsTrackBar1.TabIndex = 2
+        Me.NsTrackBar1.Text = "Opacity"
+        Me.NsTrackBar1.Value = 100
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(50, Byte), Integer), CType(CType(50, Byte), Integer), CType(CType(50, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(410, 303)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.Text = "Floating Browser"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents WebBrowser1 As System.Windows.Forms.WebBrowser
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents NsTrackBar1 As FloatingBrowser.NSTrackBar
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Panel2 As System.Windows.Forms.Panel
+    Friend WithEvents NsControlButton2 As FloatingBrowser.NSControlButton
+    Friend WithEvents NsControlButton1 As FloatingBrowser.NSControlButton
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents NsControlButton3 As FloatingBrowser.NSControlButton
+
+End Class
---
+++ Floating Browser/Form1.vb
@@ -0,0 +1,112 @@
+﻿Imports System.Runtime.InteropServices
+
+Public Class Form1
+    'Declare the variables
+    Dim drag As Boolean
+    Dim mousex As Integer
+    Dim mousey As Integer
+    Dim locked As Boolean = False
+    Private Declare Function GetForegroundWindow Lib "user32" Alias "GetForegroundWindow" () As IntPtr
+    Private Declare Auto Function GetWindowText Lib "user32" (ByVal hWnd As System.IntPtr, ByVal lpString As System.Text.StringBuilder, ByVal cch As Integer) As Integer
+    Dim youtubeurl As String = "http://www.youtube.com/embed/[VIDEOID]?autohide=1&autoplay=1&fs=0&iv_load_policy=3&showinfo=0&rel=0&cc_load_policy=1"
+
+    Private Sub Form1_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Panel2.MouseDown, Panel1.MouseDown
+        If e.Button = MouseButtons.Left Then
+            drag = True
+            mousex = Windows.Forms.Cursor.Position.X - Me.Left
+            mousey = Windows.Forms.Cursor.Position.Y - Me.Top
+        End If
+    End Sub
+
+    Private Sub Form1_MouseMove(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Panel2.MouseMove, Panel1.MouseMove
+        If drag Then
+            Me.Top = Windows.Forms.Cursor.Position.Y - mousey
+            Me.Left = Windows.Forms.Cursor.Position.X - mousex
+        End If
+    End Sub
+    Private Sub Form1_MouseUp(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Panel2.MouseUp, Panel1.MouseUp
+        drag = False
+    End Sub
+
+    Private Sub Form1_Leave(sender As Object, e As EventArgs) Handles MyBase.Leave
+        Me.BringToFront()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.TopMost = True
+        Me.Capture = True
+        WebBrowser1.Navigate("http://cyanlabs.co.uk/external/floatingbrowser.html")
+
+    End Sub
+
+    Private Sub NsTrackBar1_Scroll(sender As Object) Handles NsTrackBar1.Scroll
+        Me.Opacity = NsTrackBar1.Value / 100
+    End Sub
+
+    Private Sub Form1_Shown(sender As Object, e As EventArgs) Handles MyBase.Shown
+        Me.MaximumSize = Screen.PrimaryScreen.WorkingArea.Size
+        NsTrackBar1.Value = Me.Opacity * 100
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Dim pt As POINTAPI
+        pt.x = -1
+        pt.y = -1
+        GetCursorPos(pt)
+        If pt.x < Location.X - 10 Or pt.y < Location.Y Or pt.x > Location.X + Width - 1 Or pt.y > Location.Y + Height - 1 Then
+            Me.Height = 248
+            Panel2.Visible = False
+        Else
+            Me.Height = 300
+            Panel2.Visible = True
+        End If
+    End Sub
+    <DllImport("User32.dll")> _
+    Public Shared Function GetCursorPos(ByRef pt As POINTAPI) As Integer
+    End Function
+
+    <StructLayout(LayoutKind.Sequential)> _
+    Public Structure POINTAPI
+        Public x As Integer
+        Public y As Integer
+    End Structure
+
+    Protected Overrides ReadOnly Property CreateParams() As CreateParams
+        Get
+            Const WS_EX_TOPMOST As Integer = &H8
+            Dim cp As CreateParams = MyBase.CreateParams
+            cp.ExStyle = cp.ExStyle Or WS_EX_TOPMOST
+            Return cp
+        End Get
+    End Property
+    Private Sub TextBox1_KeyUp(sender As Object, e As KeyEventArgs) Handles TextBox1.KeyUp
+        If e.KeyCode = Keys.Enter Then
+            e.SuppressKeyPress = True
+            e.Handled = True
+            ParseURI(TextBox1.Text)
+
+        End If
+    End Sub
+
+    Sub ParseURI(ByVal uri As String)
+
+        If uri.Contains("youtube.com/watch?v=") Then
+            uri = uri.Substring(uri.IndexOf("=") + 1)
+            If uri.Length > 11 Then uri = uri.Substring(0, uri.IndexOf("&"))
+            uri = "http://www.youtube.com/embed/" & uri & "?autohide=1&autoplay=1&fs=0&iv_load_policy=3&showinfo=0&rel=0&cc_load_policy=1"
+
+            'Dim ele = (From X In WebBrowser1.Document.GetElementsByTagName("div").Cast(Of HtmlElement)() Where X.GetAttribute("id") = "player-api" Select X).FirstOrDefault
+            'If ele IsNot Nothing Then ele.Style = "Display:none"
+            'MsgBox(uri)
+            'WebBrowser1.DocumentText = WebBrowser1.Document.GetElementById("player-api").OuterHtml.Replace("<div id=""player-api""", "<div id=""player-api"" style=""width:410px;height:249px""")
+            'MsgBox(WebBrowser1.Document.GetElementById("player-api").InnerHtml)
+        ElseIf uri = "about:floatingbrowser" Then
+            uri = "http://cyanlabs.co.uk/external/floatingbrowser.html"
+        End If
+        WebBrowser1.Navigate(uri)
+    End Sub
+
+    Private Sub TextBox1_KeyDown(sender As Object, e As KeyEventArgs) Handles TextBox1.KeyDown
+        e.SuppressKeyPress = (e.KeyCode = Keys.Enter)
+    End Sub
+End Class
